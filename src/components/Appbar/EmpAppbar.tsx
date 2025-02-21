10455f7b178a24d5133a99a3352e01e05ee8b3d8
yash gupta <yashgtech00@gmail.com>
2025-02-21 22:50:39 +0530

    add img

--- src/components/Appbar/EmpAppbar.tsx
+++ src/components/Appbar/EmpAppbar.tsx
@@ -12,7 +12,7 @@ export function EmpAppbar() {
   return (
     <div className=" fixed z-30 top-0 left-0 right-0  flex justify-between items-center px-5 py-4 md:px-10 xl:px-20 bg-white shadow-md">
       <div className="text-emerald-500 font-bold text-3xl">
-        <Link href={"/"}>JobSync</Link>
+        <Link href={"/"}><img src="https://i.ibb.co/PsZFynfm/download.png" className="w-[130px]"/></Link>
       </div>
       <div className="flex justify-between  w-[25%]">
         <div className=" font-semibold hover:text-emerald-500  text-xl hover:underline">
